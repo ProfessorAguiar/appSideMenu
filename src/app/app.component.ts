80d46cd800f52ab225a15df675b267fe0f604e5b
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-12-06 16:47:13 -0300

    0612

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Home', url: '/folder/home', icon: 'home' },
+    { title: 'Produtos', url: '/folder/produtos', icon: 'bag-handle' },
+    { title: 'Promoções', url: '/folder/promocoes', icon: 'pricetags' },
+    { title: 'Lojas', url: '/folder/lojas', icon: 'storefront' },
+    { title: 'Perfil', url: '/folder/perfil', icon: 'person-circle' },
+    { title: 'Contato', url: '/folder/contato', icon: 'call' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  constructor() {}
+}
