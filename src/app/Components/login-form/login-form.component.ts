f1a498c19ffce10a0413a69b95724e430b53e1cf
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-12-08 15:44:21 -0300

    Sem Node_modules

--- src/app/Components/login-form/login-form.component.ts
+++ src/app/Components/login-form/login-form.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent  implements OnInit {
   cad:boolean=false
+  mensagem:string=''
+  cadUser(email:any, senha:any, rpSenha:any){
+    this.mensagem=''
+    if(email=='' || senha=='' || rpSenha==''){
+      this.mensagem='Preencha todos os campos do formulário!'
+    }else if(senha!=rpSenha){
+      this.mensagem='As senhas precisam ser iguas!'
+    }else{
+      this.mensagem='Usuário cadastrado com sucesso!'
+    }
+
+  }
+
+  Logar(email:any, senha:any){
+    console.log(`Usuário: ${email} logado com senha: ${senha} `)
+  }
+
   constructor() { }
 
   ngOnInit() {}
