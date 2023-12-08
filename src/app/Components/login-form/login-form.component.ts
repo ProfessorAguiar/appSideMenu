ecc01b606f7fc1bcf0faca53028e484ef0ae3171
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-12-08 16:56:17 -0300

    final da aula do dia 0812

--- src/app/Components/login-form/login-form.component.ts
+++ src/app/Components/login-form/login-form.component.ts
@@ -1,31 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
 })
-export class LoginFormComponent  implements OnInit {
-  cad:boolean=false
-  mensagem:string=''
-  cadUser(email:any, senha:any, rpSenha:any){
-    this.mensagem=''
-    if(email=='' || senha=='' || rpSenha==''){
-      this.mensagem='Preencha todos os campos do formulário!'
-    }else if(senha!=rpSenha){
-      this.mensagem='As senhas precisam ser iguas!'
-    }else{
-      this.mensagem='Usuário cadastrado com sucesso!'
+export class LoginFormComponent implements OnInit {
+  cad: boolean = false
+  mensagem: string = ''
+  cadUser(email: any, senha: any, rpSenha: any) {
+    this.mensagem = ''
+    if (email == '' || senha == '' || rpSenha == '') {
+      this.mensagem = 'Preencha todos os campos do formulário!'
+    } else if (senha != rpSenha) {
+      this.mensagem = 'As senhas precisam ser iguas!'
+    } else {
+      this.mensagem = 'Usuário cadastrado com sucesso!'
+      
+      createUserWithEmailAndPassword(this.auth, email, senha)
+        .then((userCredential) => {
+          // Signed up 
+          const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+        });
     }
 
   }
 
-  Logar(email:any, senha:any){
+  Logar(email: any, senha: any) {
     console.log(`Usuário: ${email} logado com senha: ${senha} `)
   }
 
-  constructor() { }
+  constructor(private auth:Auth) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
+
+
+
+
+
