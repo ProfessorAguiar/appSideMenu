af736d7b5c58920784d7ceb7edd5506f9626c186
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-12-15 16:57:41 -0300

    final

--- src/app/Pages/produtos/produtos.page.ts
+++ src/app/Pages/produtos/produtos.page.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { getStorage, ref, listAll, Storage, getDownloadURL } from '@angular/fire/storage';
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.page.html',
   styleUrls: ['./produtos.page.scss'],
 })
 export class ProdutosPage implements OnInit {
-
-  constructor() { }
+  images:any=[]
+  constructor(private storage:Storage) { }
 
   ngOnInit() {
+    this.listarProdutos()
   }
 
+  listarProdutos() {
+    const listRef = ref(this.storage, 'Produtos');
+    listAll(listRef)
+      .then((res) => {
+        res.items.forEach((itemRef) => {
+          getDownloadURL(itemRef).then((res) => {
+          this.images.push(res)
+          })
+        });
+      }).catch((error) => {
+      });
+  }
 }
+
+// listAll(ref(this.af, 'cardapio')).then(imgs => {
+//   imgs.items.forEach((im) => {
+//     //console.log(im.fullPath)
+//     //console.log(im.bucket)
+//     getDownloadURL(im).then((res) => {
+//       //console.log(res)
+//       this.cardapioImages.push(res)
+//     })
+//   })
+// })
