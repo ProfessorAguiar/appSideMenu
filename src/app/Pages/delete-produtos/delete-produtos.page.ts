464719cb1f0ca223327c8d9284333ba4d3c925be
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2024-01-10 14:56:29 -0300

    intervalo

--- src/app/Pages/delete-produtos/delete-produtos.page.ts
+++ src/app/Pages/delete-produtos/delete-produtos.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { collection, Firestore, getDocs, deleteDoc, doc } from '@angular/fire/firestore';
+import { getStorage, ref, listAll, Storage, getDownloadURL } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-delete-produtos',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-produtos.page.scss'],
 })
 export class DeleteProdutosPage implements OnInit {
-
-  constructor() { }
-
+  isToastOpen = false;
+  produtos:any = []
+  constructor(private storage:Storage, private firestore:Firestore) { }
   ngOnInit() {
+    this.listarBanco()
+  }
+  async listarBanco() {
+    const querySnapshot = await getDocs(collection(this.firestore, "Produtos"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()['nome']}`);
+      this.produtos = [...this.produtos, { id: doc.id, nome: doc.data()['nome'], descricao: doc.data()['descricao'], preco: doc.data()['preco'], qtd: doc.data()['qtd'], image: doc.data()['image'] }]
+    });
   }
 
+  mensagem(isOpen: boolean){
+    this.isToastOpen = isOpen;
+  }
+
+  async DeletarItem(isOpen: boolean, id:string){
+    await deleteDoc(doc(this.firestore, "Produtos", id));
+    this.mensagem(isOpen)
+    setTimeout(() => {
+      this.produtos=[]
+      this.listarBanco()
+     }, 2000);
+  }
+  
 }
+
