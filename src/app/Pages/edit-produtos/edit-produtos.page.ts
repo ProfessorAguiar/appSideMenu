ba8645ff9ca5e98a0cee4d92b1b036e865e0c4d5
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2024-01-12 14:30:47 -0300

    14:30

--- src/app/Pages/edit-produtos/edit-produtos.page.ts
+++ src/app/Pages/edit-produtos/edit-produtos.page.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-
+import { collection, Firestore, getDocs, deleteDoc, doc } from '@angular/fire/firestore';
+import { getStorage, ref, listAll, Storage, getDownloadURL } from '@angular/fire/storage';
 @Component({
   selector: 'app-edit-produtos',
   templateUrl: './edit-produtos.page.html',
   styleUrls: ['./edit-produtos.page.scss'],
 })
 export class EditProdutosPage implements OnInit {
+  isToastOpen = false;
+  produtos: any = []
 
-  constructor() { }
-
+  public alertButtons = ['OK'];
+  public alertInputs = [
+    {
+      placeholder: 'Name',
+    },
+    {
+      placeholder: 'Nickname (max 8 characters)',
+      attributes: {
+        maxlength: 8,
+      },
+    },
+    {
+      type: 'number',
+      placeholder: 'Age',
+      min: 1,
+      max: 100,
+    },
+    {
+      type: 'textarea',
+      placeholder: 'A little about yourself',
+    },
+  ];
+  
+  constructor(private storage: Storage, private firestore: Firestore) { }
   ngOnInit() {
+    this.listarBanco()
+  }
+  async listarBanco() {
+    const querySnapshot = await getDocs(collection(this.firestore, "Produtos"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()['nome']}`);
+      this.produtos = [...this.produtos, { id: doc.id, nome: doc.data()['nome'], descricao: doc.data()['descricao'], preco: doc.data()['preco'], qtd: doc.data()['qtd'], image: doc.data()['image'] }]
+    });
+  }
+
+  mensagem(isOpen: boolean) {
+    this.isToastOpen = isOpen;
+  }
+
+  async EditarItem(isOpen: boolean, id: string) {
+    await deleteDoc(doc(this.firestore, "Produtos", id));
+    this.mensagem(isOpen)
+    setTimeout(() => {
+      this.produtos = []
+      this.listarBanco()
+    }, 2000);
   }
 
 }
+
+
+
+
+
+
+
+
